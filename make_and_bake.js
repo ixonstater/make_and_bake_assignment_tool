2f136584a45235b29f9fa26630057ff7b9f943db
Josh Jarvis <ixonstater@gmail.com>
2019-11-24 17:20:33 -0700

    Finished project

--- make_and_bake.js
+++ make_and_bake.js
@@ -44,6 +44,18 @@ function getElemsFromClick(e){
     return [inputTextId, appendPointSelector]
 }
 
+function validateForSubmission(elemId ,text){
+    let src = elemId.split('-')[0]
+
+    if(text == ''){
+        throw ('You can\' submit an empty name.')
+    } else if (src == 'sender' && progData.sendersList.includes(text)){
+        throw ('You can\'t submit duplicate names.')
+    } else if (src == 'reciever' && progData.recieversList.includes(text)){
+        throw ('You can\'t submit duplicate names.')
+    }
+}
+
 function submitButtonClicked(e){
     let inputTextId
     let appendPointSelector
@@ -51,10 +63,14 @@ function submitButtonClicked(e){
     
     let input = document.getElementById(inputTextId)
     let inputText = input.value
-    if(inputText == ''){
+    input.value = ''
+    try{
+        validateForSubmission(inputTextId, inputText)
+    }
+    catch (err){
+        alert(err)
         return
     }
-    input.value = ''
 
     let appendPoint = document.querySelector(appendPointSelector)
     let nodes = manageInput(inputText, inputTextId.split('-')[0])
@@ -113,6 +129,22 @@ function shuffleArray(arr){
 function makeMatchesData(){
     progData.matches.recievers = shuffleArray(progData.recieversList)
     progData.matches.senders = shuffleArray(progData.sendersList)
+    for (let i = 0; i < progData.matches.recievers.length; i++) {
+        if(progData.matches.recievers[i] == progData.matches.senders[i]){
+            if(progData.matches.recievers.length == 1){
+                break;
+            }
+            else if(i == 0){
+                let temp = progData.matches.senders[i]
+                progData.matches.senders[i] = progData.matches.senders[i + 1]
+                progData.matches.senders[i + 1] = temp
+                continue
+            }
+            let temp = progData.matches.senders[i]
+            progData.matches.senders[i] = progData.matches.senders[i - 1]
+            progData.matches.senders[i - 1] = temp
+        }
+    }
 }
 
 function makeMatchesDom(){
@@ -131,24 +163,85 @@ function makeMatchesDom(){
     return [sendersList, recieversList]
 }
 
-function showMatches(matches){
+function showMatches(sendersList, recieversList){
     document.querySelector('#match-list-container > table').className = 'page-table'
+    let sendersAppendPoint = document.querySelector('#senders-match-list ol')
+    let recieversAppendPoint = document.querySelector('#recievers-match-list ol')
+    for (let elem of sendersList){
+        sendersAppendPoint.appendChild(elem)
+    }
+    for (let elem of recieversList){
+        recieversAppendPoint.appendChild(elem)
+    }
 }
 
-function matchButtonClicked(e){
+function matchSubmitButtonClicked(e){
     try{
         validateForMatch()
     }
     catch(err){
         alert(err)
+        return
+    }
+    try{
+        clearMatchDom()
+        clearMatchProgData()
+    }
+    catch(err){
+        
     }
     makeMatchesData()
     let [sendersList, recieversList] = makeMatchesDom()
     showMatches(sendersList, recieversList)
 }
 
-function clearMatchButtonClicked(e){
+function clearMatchProgData(){
+    progData.matches.recievers = []
+    progData.matches.senders = []
+}
 
+function clearMatchDom(){
+    let sendersList = document.querySelector('#senders-match-list ol')
+    let recieversList = document.querySelector('#recievers-match-list ol')
+    while(sendersList.firstChild){
+        sendersList.removeChild(sendersList.firstChild)
+    }
+    while(recieversList.firstChild){
+        recieversList.removeChild(recieversList.firstChild)
+    }
+}
+
+function hideMatches(){
+    document.querySelector('#match-list-container > table').className = 'hidden'
+}
+
+function matchClearButtonClicked(e){
+    clearMatchDom()
+    clearMatchProgData()
+    hideMatches()
+}
+
+function printButtonClicked(e){
+    let doc = new jsPDF('landscape')
+    let source = document.getElementById('match-list-container')
+    let senderOffsetX = 65
+    let recieverOffsetX = 165
+    let offsetY = 20
+    doc.setTextColor(0,100,0);
+    doc.setFontSize(30)
+    doc.text(senderOffsetX, 20, 'Gift Senders')
+    doc.text(recieverOffsetX, 20, 'Gift Recievers')
+
+    doc.setFontSize(15)
+    doc.setTextColor(200, 0, 0)
+    for (let i = 0; i < progData.matches.senders.length; i++){
+        doc.text(senderOffsetX + 10, offsetY * (i + 2), `${i}) ${progData.matches.senders[i]}`)
+    }
+
+    for (let i = 0; i < progData.matches.recievers.length; i++){
+        doc.text(recieverOffsetX + 10, offsetY * (i + 2), `${i}) ${progData.matches.recievers[i]}`)
+    }
+    doc.output('dataurlnewwindow')
 }
 
 function init(){
@@ -156,8 +249,9 @@ function init(){
     document.getElementById('sender-submit').addEventListener('click', submitButtonClicked)
     document.getElementById('reciever-clear').addEventListener('click', clearButtonClicked)
     document.getElementById('sender-clear').addEventListener('click', clearButtonClicked)
-    document.getElementById('match-submit').addEventListener('click', matchButtonClicked)
-    document.getElementById('match-clear').addEventListener('click', clearButtonClicked)
+    document.getElementById('match-submit').addEventListener('click', matchSubmitButtonClicked)
+    document.getElementById('match-clear').addEventListener('click', matchClearButtonClicked)
+    document.getElementById('print').addEventListener('click', printButtonClicked)
 }
 
 document.addEventListener('DOMContentLoaded', init)
